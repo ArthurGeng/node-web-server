4c27a49cff509bc0e4471b201c8c0f9ca89b9359
Arthur Geng <Arthur@Arthurs-MacBook-Pro.local>
2017-03-12 08:17:19 +0200

    second commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const path = require('path');
-const publicPath = path.join(__dirname,'/../public');
+const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
 const express = require('express');
 
 const app = express();
 app.use(express.static(publicPath));
-app.listen(3000,()=>console.log('Running on port 3000'));
+app.listen(port,()=>console.log(`Server is up on ${port}`));
